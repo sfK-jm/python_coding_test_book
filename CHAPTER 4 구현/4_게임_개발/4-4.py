6a962305e1876178b8b7067677ef7e9a293f8fe8
sfK <noonsung@daum.net>
2022-09-10 00:24:19 +0900

    python

--- CHAPTER 4 구현/4_게임_개발/4-4.py
+++ CHAPTER 4 구현/4_게임_개발/4-4.py
@@ -41,4 +41,19 @@ while True:
         continue
     # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
     else:
-        turn_time += 1
+        turn_time += 1\
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
+
+# 정답 출력
+print(count)
