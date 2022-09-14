923ff09b17ca5164fea90b51068ed4d0372be8f4
sfK <noonsung@daum.net>
2022-09-14 23:27:43 +0900

    python

--- CHAPTER 5 DFS,BFS/2_미로_탈출/5-2.py
+++ CHAPTER 5 DFS,BFS/2_미로_탈출/5-2.py
@@ -9,7 +9,7 @@ for i in range(n):
 
 # 이동할 네 방향 정의 (상, 하, 좌, 우)
 dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, -1]
+dy = [0, 0, -1, 1]
 
 # BFS 소스코드 구현
 def bfs(x, y):
@@ -17,3 +17,24 @@ def bfs(x, y):
     queue = deque()
     queue.append((x, y))
     # 큐가 빌 때까지 반복
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 네 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if gragh[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if gragh[nx][ny] == 1:
+                gragh[nx][ny] = gragh[x][y] + 1
+                queue.append((nx, ny))
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    return gragh[n - 1][m - 1]
+
+# BFS를 수행한 결과 출력
+print(bfs(0, 0))
